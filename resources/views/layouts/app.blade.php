4dcbc6b0e19da504d0d1e10356f06964ba65c159
匚卄乇卂ㄒ卄 <cheathbo@gmail.com>
2026-05-21 15:12:53 +0700

    Update page title and header for app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Simple Laravel 12 CRUD Application Tutorial - AllPHPTricks.com</title>
+    <title>Laravel 12 CRUD </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" crossorigin="anonymous">
 </head>
 <body>   
 
     <div class="container">
-        <h3 class=" mt-3">Simple Laravel 12 CRUD Application Tutorial - <a href="https://www.allphptricks.com/">AllPHPTricks.com</a></h3>
+        <h3 class=" mt-3">CHEATHM-Testing - <a href="https://www.allphptricks.com/">AllPHPTricks.com</a></h3>
             @yield('content')
             <div class="row justify-content-center text-center mt-3">
                 <div class="col-md-12">
